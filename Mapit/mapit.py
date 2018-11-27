3d752f8c233b59fea4954b1611462f58bb8e6fab
ayush <atulg7516@gmail.com>
2018-11-27 22:43:03 +0530

    mapit

--- Mapit/mapit.py
+++ Mapit/mapit.py
@@ -1,1 +1,10 @@
+import sys
+import webbrowser
+import pyperclip
 
+if len(sys.argv) > 1:
+    address = ' '.join(sys.argv[1:])
+
+else:
+    address = pyperclip.paste()
+webbrowser.open('https://www.google.com/maps/place/' + address)
